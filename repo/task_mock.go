872c0b365ad8bbbdfefcf9fd1b969335e8d398fa
Venkatraman Kannan <venkatramankannantech@gmail.com>
2024-10-24 22:27:32 +0530

    implement repo mocks

--- repo/task_mock.go
+++ repo/task_mock.go
@@ -11,22 +11,22 @@ type TaskRepoMock struct {
 	mock.Mock
 }
 
-func (t TaskRepoMock) AddTask(ctx *gin.Context, task *domain.Task) error {
-	//TODO implement me
-	panic("implement me")
+func (t *TaskRepoMock) AddTask(ctx *gin.Context, task *domain.Task) error {
+	args := t.Called(ctx, task)
+	return args.Error(0)
 }
 
-func (t TaskRepoMock) EditTask(ctx *gin.Context, task *domain.Task) error {
-	//TODO implement me
-	panic("implement me")
+func (t *TaskRepoMock) EditTask(ctx *gin.Context, task *domain.Task) error {
+	args := t.Called(ctx, task)
+	return args.Error(0)
 }
 
-func (t TaskRepoMock) GetAllTasksForUser(ctx *gin.Context, userId int64) ([]domain.Task, error) {
-	//TODO implement me
-	panic("implement me")
+func (t *TaskRepoMock) GetAllTasksForUser(ctx *gin.Context, userId int64) ([]domain.Task, error) {
+	args := t.Called(ctx, userId)
+	return args.Get(0).([]domain.Task), args.Error(1)
 }
 
 func (t TaskRepoMock) GetTaskById(ctx *gin.Context, id primitive.ObjectID) (*domain.Task, error) {
-	//TODO implement me
-	panic("implement me")
+	args := t.Called(ctx, id)
+	return args.Get(0).(*domain.Task), args.Error(1)
 }
